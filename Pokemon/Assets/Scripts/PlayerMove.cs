a8bf9604acd5d1b5471a0efba7fb2d4d51f3788d
DascalRaul <dascalraulsebastian15@gmail.com>
2025-10-27 14:32:21 +0200

    Made The Moving Animations Fully Functional

--- Pokemon/Assets/Scripts/PlayerMove.cs
+++ Pokemon/Assets/Scripts/PlayerMove.cs
@@ -1,16 +1,31 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerMove : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public int speed;
+    public Animator anim;
+    SpriteRenderer sprite;
     void Start()
     {
-        
+        sprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        Vector2 moveDir = new Vector2(moveX, moveY).normalized;
+        transform.Translate(moveDir * speed * Time.deltaTime);
+
+        if (moveX != 0)
+            sprite.flipX = moveX < 0;
+
+        anim.SetBool("isRunning", moveX != 0);
+        anim.SetBool("isGoingUp", moveY > 0);
+        anim.SetBool("isGoingD", moveY < 0);
     }
 }
